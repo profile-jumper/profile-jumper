0fe6fb69979676fc0dd9751abd3a85731763c18c
mrupgradable <me@mrupgradable.me>
2024-03-12 23:42:14 +0000

    refactoring out routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
-import Popup from './layout/popup/Popup'
-import Settings from './layout/settings/Settings'
+import { AppRoutes } from './route/AppRoutes'
 
-function App() {
-  return (
-    <React.Fragment>
-        <Routes>
-            <Route path="/" element={<Navigate to="/popup" replace />}/>
-            <Route path="/popup" element={<Popup/>}/>
-            <Route path="/settings" element={<Settings/>}/>
-        </Routes>
-    </React.Fragment>
-  )
-}
+const App = () => (
+    <HashRouter>
+        <AppRoutes/>
+    </HashRouter>
+)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 
 import './index.css'
 
-import App from './App';
+import App from './App'
 
-const app= (
-    <HashRouter>
-        <App />
-    </HashRouter>
-)
-
-createRoot(document.getElementById('root')).render(app)
+createRoot(document.getElementById('root')).render(<App />)
