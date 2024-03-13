0d49851ede0e0bc2020251ef9cea6dc3d34eb1a1
mrupgradable <me@mrupgradable.me>
2024-03-13 23:17:05 +0000

    removed old validation

--- src/component/setting/setting-profiles/setting-profile/profile-url/ProfileUrl.js
+++ src/component/setting/setting-profiles/setting-profile/profile-url/ProfileUrl.js
@@ -1,42 +1,28 @@
-import React, { Component } from 'react'
-
-import { checkValidity } from '../../../utility/inputValidation'
+import React from 'react'
 
 import './ProfileUrl.css'
 
 export const PROFILE_URL = 'profileUrl'
 
-export const ProfileUrl = ({register, errors}) => {
+export const ProfileUrl = ({ register, errors }) => {
     const inputStyle = (errors[PROFILE_URL]) ? 'error' : null
-    const errorShow = (errors[PROFILE_URL]) ? <p className='error'>{errors[PROFILE_URL]?.message}</p> : null
-
-    // checkInputValidity = (updatedValue) => {
-    //   const profileUrlInput = this.state.profileUrlInput
-    //
-    //   if(updatedValue) profileUrlInput.value = updatedValue
-    //
-    //   profileUrlInput.valid = checkValidity(profileUrlInput.value, profileUrlInput.validation)
-    //   profileUrlInput.error = (!profileUrlInput.valid && profileUrlInput.touched)
-    //
-    //   const formIsValid = profileUrlInput.valid
-    //
-    //   if(formIsValid) {
-    //     this.props.updateValue(profileUrlInput.value, formIsValid)
-    //   }
-    // }
+    const errorShow = (errors[PROFILE_URL]) ? <p className='error'>{ errors[PROFILE_URL]?.message }</p> : null
 
     return (
         <div className="ProfileUrl">
-            {errorShow}
+            { errorShow }
             <input type="text"
-                   name="profileUrl"
                    placeholder="Link e.g. https://instagram.com/mrupgradable"
                    autoComplete="off"
-                   className={inputStyle}
-                   {...register(PROFILE_URL, {
+                   className={ inputStyle }
+                   { ...register(PROFILE_URL, {
                        required: 'Enter or paste link URL e.g. https://instagram.com/mrupgradable',
-                       minLength: {value: 5, message: 'URL Link is too short, enter or paste it here'}
-                   })}
+                       minLength: { value: 5, message: 'URL link is too short, enter or paste it here' },
+                       pattern: {
+                           value: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/gi,
+                           message: 'Link is incorrect, must be valid URL e.g. https://x.com/mr_upgradable'
+                       }
+                   }) }
             />
         </div>
     )
