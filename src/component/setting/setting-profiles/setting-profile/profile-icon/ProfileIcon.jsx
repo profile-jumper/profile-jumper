17360cb176d9bdff6bdcc200884bc6f33f38cc83
mrupgradable <me@mrupgradable.me>
2024-04-02 17:37:26 +0100

    added simple colour picker

--- src/component/setting/setting-profiles/setting-profile/profile-icon/ProfileIcon.jsx
+++ src/component/setting/setting-profiles/setting-profile/profile-icon/ProfileIcon.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import { findProfileIcon, smallIconSize } from '../../../../../utility/profile/profile-icon'
+import { ColorPicker } from '../../../../color-picker/ColorPicker'
 
 import './ProfileIcon.css'
 
-export const ProfileIcon = ({ icon }) => (
-    <div className="SettingProfileIconWrapper">
-        <FontAwesomeIcon icon={ findProfileIcon(icon) } size={ smallIconSize } className="SettingProfileIcon"/>
-    </div>
-)
+export const ProfileIcon = ({ icon }) => {
+    const [showColorPicker, setShowColorPicker] = useState(false)
+
+    const onColorChoose = (color) => {
+        console.log('color!', color)
+        toggleColorPicker()
+    }
+
+    const toggleColorPicker = () => setShowColorPicker(!showColorPicker)
+
+    return (
+        <div className="SettingProfileIconWrapper">
+            <ColorPicker visible={showColorPicker} onColorChoose={onColorChoose}/>
+            <FontAwesomeIcon icon={ findProfileIcon(icon) } size={ smallIconSize } className="SettingProfileIcon" onClick={toggleColorPicker}/>
+        </div>
+    )
+}
