094cb89418decca2ae900e333301d30b413067ec
mrupgradable <me@mrupgradable.me>
2024-04-03 16:15:30 +0100

    extracting name from url

---
+++ src/__tests__/utility/url-utility.spec.js
@@ -0,0 +1,30 @@
+import { scrubUrlParts } from '../../utility/url/url-utility'
+
+describe('URL Utility', () => {
+
+    test('should obtain basic domain info without protocol or tld', () => {
+        expect(scrubUrlParts('https://google.com')).toEqual('google')
+        expect(scrubUrlParts('https://test.co.uk')).toEqual('test')
+    })
+
+    test('should not exact url if invalid', () => {
+        expect(scrubUrlParts(undefined)).toBeUndefined()
+        expect(scrubUrlParts(null)).toBeNull()
+        expect(scrubUrlParts('')).toEqual('')
+        expect(scrubUrlParts('   ')).toEqual('   ')
+        expect(scrubUrlParts('some-word')).toEqual('some-word')
+    })
+
+    test('should obtain smallest name from url', () => {
+        expect(scrubUrlParts('https://x.com')).toEqual('x')
+    })
+
+    test('should obtain name from url when trailing parts like parameters', () => {
+        expect(scrubUrlParts('https://x.com/mr_upgradable')).toEqual('x')
+        expect(scrubUrlParts('https://twitter.com/mr_upgradable')).toEqual('twitter')
+        expect(scrubUrlParts('https://www.instagram.com/mrupgradable')).toEqual('instagram')
+        expect(scrubUrlParts('https://www.instagram.com/mrupgradable/?hl=en')).toEqual('instagram')
+    })
+
+
+})
