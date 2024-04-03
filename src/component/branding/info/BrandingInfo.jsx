8f56a981fd260e97bcb7306e024a5197cab81c09
mrupgradable <me@mrupgradable.me>
2024-04-03 18:59:09 +0100

    updated popup text

--- src/component/branding/info/BrandingInfo.jsx
+++ src/component/branding/info/BrandingInfo.jsx
@@ -8,12 +8,19 @@ import './BrandingInfo.css'
 export const BrandingInfo = () => (
     <React.Fragment>
         <div className="BrandingInfo">
-            <h1><a href="https://profile-jumper.github.io" className="BrandingLink" target="_blank" rel="noopener noreferrer"
-                   title="Profile Jumper! - See website for info">Profile Jumper!</a> <span className="BrandingInfoVersion">({ APP_VERSION })</span></h1>
-            <p>Jump between any of your online profiles! Click on cogs or right-click icon for "options" to configure instantly.</p>
+            <h1>
+                <a href="https://profile-jumper.github.io"
+                   className="BrandingLink"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                   title="Profile Jumper! - See website for info">Profile Jumper!</a>
+                <span className="BrandingInfoVersion">Version { APP_VERSION }</span>
+            </h1>
+            <p>Jump easily between your profiles!</p>
+            <p>Click on the cogs or right-click extension icon for "options" to update your profile links instantly.</p>
         </div>
         <div className="ConfigureInfo">
-            <a className="ConfigureClick" href="#settings" target="_blank" rel="noopener noreferrer" title="Click here to configure - Profile Jumper!">
+            <a className="ConfigureClick" href="#settings" target="_blank" rel="noopener noreferrer" title="Click here to update your profile links - Profile Jumper!">
                 <DisplayIcon icon="FaCogs"/>
             </a>
         </div>
