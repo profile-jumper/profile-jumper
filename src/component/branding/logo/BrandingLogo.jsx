1a4ea0b45083f67d564aad628e9ebf0f0d89f949
mrupgradable <me@mrupgradable.me>
2024-05-23 07:24:07 +0100

    updated links

--- src/component/branding/logo/BrandingLogo.jsx
+++ src/component/branding/logo/BrandingLogo.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import ProfileJumperPopupLogo from '../../../asset/logo/profile-jumper-popup-logo.png'
 
-import { WEBSITE_LINK, WEBSITE_LINK_ALT } from '../../../config/constants'
+import { WEBSITE_LINK_ALT, WEBSITE_LINK_LOGO } from '../../../config/constants'
 
 import './BrandingLogo.css'
 
 export const BrandingLogo = () => (
     <div className="BrandingLogo">
-        <a href={ WEBSITE_LINK } target="_blank" rel="noopener noreferrer" title={ WEBSITE_LINK_ALT }>
+        <a href={ WEBSITE_LINK_LOGO } target="_blank" rel="noopener noreferrer" title={ WEBSITE_LINK_ALT }>
             <img src={ ProfileJumperPopupLogo } alt="Profile Jumper!"/>
         </a>
     </div>
