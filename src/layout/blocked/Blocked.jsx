c6e0556ec80d5936188410c3f51010735dfdc37a
mrupgradable <me@mrupgradable.me>
2025-06-05 20:00:55 +0100

    added blocked page

---
+++ src/layout/blocked/Blocked.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Blocked.css'
+
+export const Blocked = () => {
+    return (
+        <div className="Blocked">
+            <h1>Blocked</h1>
+        </div>
+    )
+}
